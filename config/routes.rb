8a62e3a559371cf44a6bef07db1ae793386f6d66
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2020-12-14 11:41:57 +0900

    change commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
-  resources :prototypes, only: [:new, :create, :show, :edit, :update]
+  patch "/prototypes/:id", to: "prototypes#update"
+  resources :prototypes, only: [:new, :create, :show, :edit, :destroy] do
+    resources :comments, only: :create
+  end
+  resources :users, only: :show
 end
